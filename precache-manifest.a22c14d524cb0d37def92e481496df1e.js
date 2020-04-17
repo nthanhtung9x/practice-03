a790fc0825183ea6cf7bb3a7176573abf1a52a19
Tùng Nguyễn Thanh <tung1831998@gmail.com>
2020-04-17 22:58:10 +0700

    Updates

---
+++ precache-manifest.a22c14d524cb0d37def92e481496df1e.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "619c80e2dd7ea1a581d32fcc27003872",
+    "url": "/practice-03/index.html"
+  },
+  {
+    "revision": "09ec7e1bcdeb9fedc6cf",
+    "url": "/practice-03/static/css/main.dacf8710.chunk.css"
+  },
+  {
+    "revision": "8da6db1556f7b698ce3d",
+    "url": "/practice-03/static/js/2.98c9376d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/practice-03/static/js/2.98c9376d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09ec7e1bcdeb9fedc6cf",
+    "url": "/practice-03/static/js/main.7d5421f1.chunk.js"
+  },
+  {
+    "revision": "97909678875da14edc57",
+    "url": "/practice-03/static/js/runtime-main.f91b7440.js"
+  },
+  {
+    "revision": "30498c86af4f791e8b05187c1baf5dd7",
+    "url": "/practice-03/static/media/audio.30498c86.svg"
+  },
+  {
+    "revision": "f8469cbb3729c266f2df980e0f6f30af",
+    "url": "/practice-03/static/media/cart.f8469cbb.svg"
+  },
+  {
+    "revision": "262e7e7eb57e87ace9d9ba368af8d5bf",
+    "url": "/practice-03/static/media/laptop.262e7e7e.svg"
+  },
+  {
+    "revision": "6867dc1d8c3f290c147c21b885e73fd2",
+    "url": "/practice-03/static/media/left-arrow.6867dc1d.svg"
+  },
+  {
+    "revision": "224f1fb66728f99c01e9dd646b42d88c",
+    "url": "/practice-03/static/media/logo.224f1fb6.png"
+  },
+  {
+    "revision": "4aaf92d0ed86d1b1165581081977af0d",
+    "url": "/practice-03/static/media/menu.4aaf92d0.svg"
+  },
+  {
+    "revision": "776eda91c85d5cf2b7a6c409d41a827e",
+    "url": "/practice-03/static/media/phone.776eda91.svg"
+  },
+  {
+    "revision": "445abfc59ed4db25bb9f34a6bab8ca88",
+    "url": "/practice-03/static/media/right-arrow.445abfc5.svg"
+  },
+  {
+    "revision": "61913e2b3861d92b3a80fdfc33f72ddd",
+    "url": "/practice-03/static/media/search.61913e2b.svg"
+  },
+  {
+    "revision": "0945778148e0f1c58e54ee3826739990",
+    "url": "/practice-03/static/media/slide1.09457781.png"
+  },
+  {
+    "revision": "06fee60a0aa47351f234719d97bd50c0",
+    "url": "/practice-03/static/media/slide2.06fee60a.png"
+  },
+  {
+    "revision": "f90cc72698a752e0c1557b96c28f0b71",
+    "url": "/practice-03/static/media/slide3.f90cc726.png"
+  },
+  {
+    "revision": "cb19f062dfb0c5f7a22df8200ac2d19c",
+    "url": "/practice-03/static/media/smart-watch.cb19f062.svg"
+  },
+  {
+    "revision": "b65aed6795a6172ef7aeebc2a5b12d9c",
+    "url": "/practice-03/static/media/tablet.b65aed67.svg"
+  }
+]);
