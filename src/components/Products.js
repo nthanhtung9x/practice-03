3a7019f05c7a5b3fc3e624ac47953d1890e10b99
Tùng Nguyễn Thanh <tung1831998@gmail.com>
2020-04-18 09:52:55 +0700

    'updates'

--- src/components/Products.js
+++ src/components/Products.js
@@ -21,28 +21,28 @@ class Products extends Component {
         let url = this.props.match.url;
         let result = null;
 		result = list.map((item, index) => {
-		return <div className="col l-2-4 m-4 c-6" key={index}>
-					<div className="product">
-						<a href=" ">
-							<img src={item.img} alt=""></img>
-						</a>
-						<article className="product__name">{item.name}</article>
-						<p className="product__price">{item.price}$</p>
-						<p className="product__price-old">{item.old_price}$</p>
-						<p className="product__text">{item.description}</p>
-						<span className="product__quanlity">Số lượng: {item.quanlity}</span>
-						<button
-							className="product__add"
-							disabled={item.quanlity === 0 ? true : false}
-							style={item.quanlity === 0 ? { backgroundColor: '#DC3545' } : { backgroundColor: '#007BFF' }}
-							onClick={() => this.AddToCart(item)}
-						>
-							{item.quanlity === 0 ? "Hết hàng" : "Thêm vào giỏ"}
-						</button>
+			return <div className="col l-2-4 m-4 c-6" key={index}>
+						<div className="product">
+							<a href=" ">
+								<img src={item.img} alt=""></img>
+							</a>
+							<article className="product__name">{item.name}</article>
+							<p className="product__price">{item.price}$</p>
+							<p className="product__price-old">{item.old_price}$</p>
+							<p className="product__text">{item.description}</p>
+							<span className="product__quanlity">Số lượng: {item.quanlity}</span>
+							<button
+								className="product__add"
+								disabled={item.quanlity === 0 ? true : false}
+								style={item.quanlity === 0 ? { backgroundColor: '#DC3545' } : { backgroundColor: '#007BFF' }}
+								onClick={() => this.AddToCart(item)}
+							>
+								{item.quanlity === 0 ? "Hết hàng" : "Thêm vào giỏ"}
+							</button>
+						</div>
 					</div>
-				</div>
-		});
-        else if (url === '/practice-03/phone') {
+			});
+        if (url === '/practice-03/phone') {
             result = list.map((item, index) => {
                 if (item.type.indexOf(url.slice(-5)) !== -1) {
                     return <div className="col l-2-4  m-4 c-6" key={index}>
